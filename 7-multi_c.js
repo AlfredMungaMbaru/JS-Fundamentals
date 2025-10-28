54a56c8fde4f30ff696bc3861514670c6dbd2724
AlfredMungaMbaru <munga21407@outlook.com>
2025-10-28 19:20:36 +0300

    Add implementation to print "C is fun" based on user-defined occurrences

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const firstArg = process.argv[2];
+const x = parseInt(firstArg);
+
+if (isNaN(x) || x <= 0) {
+  if (isNaN(x)) {
+    console.log("Missing number of occurrences");
+  }
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+}
